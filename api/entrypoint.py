75551b84552152f5c1a6f26f22cc9ea8527ae97b
Anton Berezin <gurunars@gmail.com>
2020-03-04 18:42:51 +0000

    ff

--- api/entrypoint.py
+++ api/entrypoint.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel, Field
 from typing import List, Dict
 
@@ -16,17 +16,30 @@ items: Dict = {
 }
 
 
-@api.get("/items", response_model=List[Item])
+@api.get("/items", response_model=List[Item], status_code=status.HTTP_200_OK)
 async def get_items(start_from: int = 0, limit: int = 10):
     return sorted(items.values())[start_from : start_from + limit]
 
 
-@api.get("/items/{item_id}", response_model=Item)
+@api.get("/items/{item_id}", response_model=Item, status_code=status.HTTP_200_OK)
 async def get_item(item_id: int):
+    if item_id not in items:
+        raise HTTPException(404, "Item doesn't exist")
     return items[item_id]
 
 
 @api.post("/items", status_code=status.HTTP_201_CREATED)
 async def create_item(item: Item):
-    if item.id not in items:
-        items[item.id] = item
+    if item.id in items:
+        raise HTTPException(409, "Item with such id already exists")
+    items[item.id] = item
+
+
+@api.put("/items", status_code=status.HTTP_204_NO_CONTENT)
+async def put_item(item: Item):
+    items[item.id] = item
+
+
+@api.put("/items", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_item(item_id: int):
+    items.pop(item_id, None)
