3a5c8e84e3548d43b50c39147c4a839c5c86ba3d
Anton Berezin <gurunars@gmail.com>
2020-03-04 16:01:45 +0000

    ff

--- api/entrypoint.py
+++ api/entrypoint.py
@@ -3,6 +3,21 @@ from fastapi import FastAPI
 api = FastAPI()
 
 
-@api.get("/")
-async def root():
-    return {"message": "Hello World"}
+items = {
+    42: dict(id=42, title="Title")
+}
+
+
+@api.get("/items")
+async def get_items(start_from: int = 0, limit: int = 10):
+    return sorted(items.values())[start_from : start_from + limit]
+
+
+@api.get("/items/{item_id}")
+async def get_item(item_id: int):
+    return items[item_id]
+
+
+@api.post("/items")
+async def create_item():
+    return "Created"
